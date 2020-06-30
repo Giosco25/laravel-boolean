4a6ae6e6a238b76b4bdfb498848d3113616d0a71
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-06-30 17:15:22 +0200

    Header e inizio prima parte main

---
+++ laravel-boolean/resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="{{ asset('images/logo.png') }}" alt="Boolean-logo">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Corso</a></li>
+                    <li><a href="#">Dopo il corso</a></li>
+                    <li><a href="#">Lezione Gratuita</a></li>
+                    <li><a href="#">Assumiamo i nostri studenti</a></li>
+                </ul>
+                <div class="btn">
+                    <button type="button" class="btn btn-primary">Canditati ora</button>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="first-section col-6">
+                            <h1 class="text-uppercase">Diventa sviluppatore web</h1>
+                            <p>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
+                            <ul>
+                                <li>6 mesi di corso intensivo online in diretta</li>
+                                <li>Nessuna competenza di programmazione richiesta</li>
+                                <li>Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
+                            </ul>
+                        </div>
+                        <div class="second-section col-6">
+                            <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="">
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </main>
+
+    </body>
+</html>

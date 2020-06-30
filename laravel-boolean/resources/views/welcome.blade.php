ac11076f7a82b90caa15f56359ab4d44eb159cd0
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-06-30 18:25:11 +0200

    seconda parte main

--- laravel-boolean/resources/views/welcome.blade.php
+++ laravel-boolean/resources/views/welcome.blade.php
@@ -31,20 +31,37 @@
                             <h1 class="text-uppercase">Diventa sviluppatore web</h1>
                             <p>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
                             <ul>
-                                <li>6 mesi di corso intensivo online in diretta</li>
-                                <li>Nessuna competenza di programmazione richiesta</li>
-                                <li>Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
+                            <li>6 mesi di corso intensivo online in diretta</li>
+                            <li>Nessuna competenza di programmazione richiesta</li>
+                            <li>Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
                             </ul>
                         </div>
                         <div class="second-section col-6">
                             <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="">
                         </div>
-
                     </div>
 
                 </div>
             </div>
+            <div class="number text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="number-left col-4">
+                            <h3>98%</h3>
+                            <p>La percentuale dei nostri studenti che ora lavora come web developer, oltre la metà a tempo indeterminato.</p>
+                        </div>
+                        <div class="number-center col-4">
+                            <h3>€ 23.000</h3>
+                            <p>Lo stipendio medio lordo di partenza degli studenti assunti dalle nostre aziende partner.</p>
+                        </div>
+                        <div class="number-right col-4">
+                            <h3>#1</h3>
+                            <p>Siamo il primo istituto online in Italia per indice di gradimento e risultati conseguiti.</p>
+                        </div>
+                    </div>
 
+                </div>
+            </div>
         </main>
 
     </body>
